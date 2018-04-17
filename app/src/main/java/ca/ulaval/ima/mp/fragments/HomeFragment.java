ced0cef9770bc0236d57dafbcf20d2b215686088
pain_l <louis.pain@epitech.eu>
2018-04-17 20:45:08 +0200

    [HOME] done

--- app/src/main/java/ca/ulaval/ima/mp/fragments/HomeFragment.java
+++ app/src/main/java/ca/ulaval/ima/mp/fragments/HomeFragment.java
@@ -3,6 +3,9 @@ package ca.ulaval.ima.mp.fragments;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import ca.ulaval.ima.mp.R;
 import ca.ulaval.ima.mp.activities.MainActivity;
@@ -19,7 +22,9 @@ public class HomeFragment extends AbstractFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button test = mView.findViewById(R.id.btn_test);
+        ImageView imageView = mView.findViewById(R.id.img);
+        Picasso.get().load(R.drawable.girl).into(imageView);
+        Button test = mView.findViewById(R.id.btn_auth);
         test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,7 +32,7 @@ public class HomeFragment extends AbstractFragment {
             }
         });
 
-        Button test2 = mView.findViewById(R.id.btn_test2);
+        Button test2 = mView.findViewById(R.id.btn_play);
         test2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
