959cf7f21aede346e802ddd817dfd7c31d26a503
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2018-05-04 10:53:45 -0400

    feat(TTS): TTS mute and unmute

--- app/src/main/java/ca/ulaval/ima/mp/activities/GameActivity.java
+++ app/src/main/java/ca/ulaval/ima/mp/activities/GameActivity.java
@@ -3,11 +3,18 @@ package ca.ulaval.ima.mp.activities;
 import android.os.Bundle;
 import android.os.Handler;
 import android.speech.tts.TextToSpeech;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,6 +22,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 
+import ca.ulaval.ima.mp.R;
 import ca.ulaval.ima.mp.bluetooth.BluetoothMessage;
 import ca.ulaval.ima.mp.bluetooth.BluetoothService;
 import ca.ulaval.ima.mp.bluetooth.messages.PlayerVoteMessage;
@@ -32,6 +40,7 @@ import ca.ulaval.ima.mp.game.roles.Role;
 
 abstract public class GameActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
+    private Menu menu = null;
     protected FrameLayout mFragment;
     protected Game mGame;
     protected TextToSpeech tts;
@@ -63,7 +72,6 @@ abstract public class GameActivity extends AppCompatActivity implements TextToSp
 
         mBluetoothService = new BluetoothService(this, mHandler);
         tts = new TextToSpeech(this, this);
-        this.toggleTTS(false);
     }
 
     @Override
@@ -79,18 +87,39 @@ abstract public class GameActivity extends AppCompatActivity implements TextToSp
         }
     }
 
-    public void toggleTTS(boolean start) {
-        if (this.ttsMuted) {
-            this.tts.setPitch(1.0f);
-            if (!start)
-                Toast.makeText(this, "Voix artificielle activée", Toast.LENGTH_LONG).show();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_options, menu);
+        this.menu = menu;
+        this.toggleTTS();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.mute_btn) { //or switch-case
+            this.toggleTTS();
         }
-        else {
-            this.tts.setPitch(0.0f);
-            if (!start)
-                Toast.makeText(this, "Voix artificielle désactivée", Toast.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void toggleTTS() {
+        MenuItem muteBtn = this.menu.findItem(R.id.mute_btn);
+        if (this.ttsMuted) {
+            Toast.makeText(this, "Voix artificielle activée", Toast.LENGTH_LONG).show();
+            muteBtn.setTitle(R.string.mute);
+        } else {
+            Toast.makeText(this, "Voix artificielle désactivée", Toast.LENGTH_LONG).show();
+            muteBtn.setTitle(R.string.unmute);
         }
         this.ttsMuted = !this.ttsMuted;
+    }
+
+    private void speak(String line) {
+        if (!this.ttsMuted)
+            this.tts.speak(line, TextToSpeech.QUEUE_FLUSH, null);
     }
 
     @Override
@@ -114,10 +143,8 @@ abstract public class GameActivity extends AppCompatActivity implements TextToSp
         startRolesStep(mGame.getPlayers());
     }
 
-    public boolean fragmentTransit(Fragment transit, boolean toBackStack)
-    {
-        if (transit != null)
-        {
+    public boolean fragmentTransit(Fragment transit, boolean toBackStack) {
+        if (transit != null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             transaction.replace(mFragment.getId(), transit, transit.getClass().getSimpleName());
             if (toBackStack)
@@ -140,42 +167,36 @@ abstract public class GameActivity extends AppCompatActivity implements TextToSp
         mBluetoothService.write(message);
     }
 
-    public void startRolesStep(List<Player> players)
-    {
+    public void startRolesStep(List<Player> players) {
         this.fragmentTransit(GameDuoFragment.newInstance(GameDuoFragment.CHOICE_MODE.ROLES, players), false);
     }
 
-    public void startRevealStep(Player player)
-    {
+    public void startRevealStep(Player player) {
         this.fragmentTransit(RevealRoleFragment.newInstance(player), true);
     }
 
-    public void startDebateStep(int turn)
-    {
+    public void startDebateStep(int turn) {
         String speech = "Le jour se lève sur le village.";
         speech += (turn == 0) ? "Vous sentez qu'un ou plusieurs loups-garous sont cachés parmi vous. Vous devez les trouver. Mais avant, faites connaissances." : "C'est l'heure du débat! Vous avez 10 minutes pour débusquez les intrus";
         this.fragmentTransit(GameDuoFragment.newInstance(GameDuoFragment.CHOICE_MODE.DEBATE, null), false);
-        tts.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
+        this.speak(speech);
     }
 
-    public void startWolvesStep(List<Player> wolves, List<Player> meals)
-    {
+    public void startWolvesStep(List<Player> wolves, List<Player> meals) {
         GameDuoFragment wolvesFragment = GameDuoFragment.newInstance(GameDuoFragment.CHOICE_MODE.WOLVES, wolves);
         wolvesFragment.setTargets(meals);
         this.fragmentTransit(wolvesFragment, false);
-        tts.speak("La nuit tombe sur le village... Loups-garous, réveillez-vous et choisissez votre victime", TextToSpeech.QUEUE_FLUSH, null);
+        this.speak("La nuit tombe sur le village... Loups-garous, réveillez-vous et choisissez votre victime");
     }
 
-    public void startVotesStep(List<Player> votes)
-    {
+    public void startVotesStep(List<Player> votes) {
         GameDuoFragment fragment = GameDuoFragment.newInstance(GameDuoFragment.CHOICE_MODE.VOTES, votes);
         fragment.setTargets(votes);
         this.fragmentTransit(fragment, false);
-        tts.speak("Qui sera pendu? Votez chacun votre tour", TextToSpeech.QUEUE_FLUSH, null);
+        this.speak("Qui sera pendu? Votez chacun votre tour");
     }
 
-    public void startTargetStep(Player name, List<Player> targets, TargetFragment.TARGET_MODE mode)
-    {
+    public void startTargetStep(Player name, List<Player> targets, TargetFragment.TARGET_MODE mode) {
         this.fragmentTransit(TargetFragment.newInstance(name, targets, mode), true);
     }
 
@@ -184,7 +205,7 @@ abstract public class GameActivity extends AppCompatActivity implements TextToSp
         String speech = dead.getName() + " est mort ! C'était un ";
         speech += (dead.getRole().getSide() == Role.Side.WEREWOLF) ? "loup-garou" : "villageois";
         this.fragmentTransit(DeathFragment.newInstance(deads.get(0), time), false);
-        tts.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
+        this.speak(speech);
     }
 
     public void endDeathStep(Game.Time time) {
@@ -194,36 +215,34 @@ abstract public class GameActivity extends AppCompatActivity implements TextToSp
             prepareStep(3);
     }
 
-    public void startWinStep(Role.Side winner)
-    {
+    public void startWinStep(Role.Side winner) {
         String speech = (winner == Role.Side.WEREWOLF) ? "Les loups-garous ont gagnés!" : "Les villageois ont gagnés!";
         this.fragmentTransit(WinFragment.newInstance(winner), false);
-        tts.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
+        this.speak(speech);
     }
 
-    public Game getGame()
-    {
+    public Game getGame() {
         return mGame;
     }
 
     protected void interpretMessage(BluetoothMessage message) {
-        Log.d("MESSAGE", "TYPE : "  + message.type);
+        Log.d("MESSAGE", "TYPE : " + message.type);
         switch (message.type) {
             case ROLE_DISPATCH:
-                RoleDispatchMessage dispatchMessage = (RoleDispatchMessage)message.content;
+                RoleDispatchMessage dispatchMessage = (RoleDispatchMessage) message.content;
                 Log.d("ROLE DISPATCH", "ROLES : " + dispatchMessage.roles);
                 startGame(dispatchMessage.roles);
                 break;
             case STEP_CHANGE:
-                StepChangeMessage stepChangeMessage = (StepChangeMessage)message.content;
-                Log.d("STEP CHANGE", "STEP : "  + stepChangeMessage.stepId);
+                StepChangeMessage stepChangeMessage = (StepChangeMessage) message.content;
+                Log.d("STEP CHANGE", "STEP : " + stepChangeMessage.stepId);
                 mGame.play(stepChangeMessage.stepId);
                 break;
             case PLAYER_VOTE:
-                PlayerVoteMessage voteMessage = (PlayerVoteMessage)message.content;
-                Log.d("PLAYER VOTE", "FROM : "  + voteMessage.playerId + " TO : " + voteMessage.targetId);
+                PlayerVoteMessage voteMessage = (PlayerVoteMessage) message.content;
+                Log.d("PLAYER VOTE", "FROM : " + voteMessage.playerId + " TO : " + voteMessage.targetId);
                 mGame.playerVote(voteMessage.playerId, voteMessage.targetId);
-                ((AbstractFragment)getSupportFragmentManager()
+                ((AbstractFragment) getSupportFragmentManager()
                         .findFragmentById(mFragment.getId())).onBluetoothResponse();
                 break;
         }
