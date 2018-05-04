6f8be69fc72dee68ba3037a13bef78b3e2335970
pain_l <louis.pain@epitech.eu>
2018-05-04 23:16:55 +0200

    STATS ok

--- app/src/main/java/ca/ulaval/ima/mp/fragments/StatsFragment.java
+++ app/src/main/java/ca/ulaval/ima/mp/fragments/StatsFragment.java
@@ -1,11 +1,26 @@
 package ca.ulaval.ima.mp.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import ca.ulaval.ima.mp.R;
+import ca.ulaval.ima.mp.application.BlueGarouApplication;
+import ca.ulaval.ima.mp.network.WSResponse;
 
 public class StatsFragment extends AbstractFragment {
+    private View AuthLayout;
+    private View NoAuthLayout;
 
     public static StatsFragment newInstance()
     {
@@ -14,8 +29,59 @@ public class StatsFragment extends AbstractFragment {
         return statsFragment;
     }
 
+    private void init()
+    {
+        AuthLayout.setVisibility(BlueGarouApplication.getInstance().getAuth() ? View.VISIBLE : View.GONE);
+        NoAuthLayout.setVisibility(BlueGarouApplication.getInstance().getAuth() ? View.GONE : View.VISIBLE);
+        SharedPreferences sharedPreferences = BlueGarouApplication.getInstance().getSharedPreferences("BLUEGAROU", Context.MODE_PRIVATE);
+        BlueGarouApplication.getInstance().getWebService().GETRequestWithAuth("stats", onStats(), sharedPreferences.getString("access_token", ""));
+    }
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        AuthLayout = mView.findViewById(R.id.auth_layout);
+        NoAuthLayout = mView.findViewById(R.id.no_auth_layout);
+        init();
+    }
+
+    private void updateStats(int nbWins, int nbVWins, int nbWWins)
+    {
+        TextView games = AuthLayout.findViewById(R.id.games);
+        TextView villagers = AuthLayout.findViewById(R.id.villagers);
+        TextView wwolfs = AuthLayout.findViewById(R.id.wwolfs);
+        games.setText(games.getText().toString().replace("--", String.valueOf(nbWins)));
+        villagers.setText(villagers.getText().toString().replace("--", String.valueOf(nbVWins)));
+        wwolfs.setText(wwolfs.getText().toString().replace("--", String.valueOf(nbWWins)));
+    }
+
+    private WSResponse<JSONObject> onStats() {
+        return new WSResponse<>(new Response.Listener<JSONObject>() {
+            int vWins = 0;
+            int wWins = 0;
+            @Override
+            public void onResponse(final JSONObject response) {
+                Log.d("GET_OBJX", response.toString());
+                try {
+                    JSONArray stats = response.getJSONArray("stats");
+                    for (int i = 0; i < stats.length(); i++)
+                    {
+                        String winning = ((JSONObject)stats.get(i)).getString("winningside");
+                        if (winning.equals("VILLAGERS"))
+                            vWins++;
+                        else
+                            wWins++;
+                    }
+                    updateStats(stats.length(), vWins, wWins);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("GET_OBJX", error.toString());
+            }
+        });
     }
 }
--- app/src/main/java/ca/ulaval/ima/mp/fragments/WinFragment.java
+++ app/src/main/java/ca/ulaval/ima/mp/fragments/WinFragment.java
@@ -1,12 +1,27 @@
 package ca.ulaval.ima.mp.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import ca.ulaval.ima.mp.R;
+import ca.ulaval.ima.mp.activities.GameActivity;
+import ca.ulaval.ima.mp.application.BlueGarouApplication;
+import ca.ulaval.ima.mp.game.Game;
+import ca.ulaval.ima.mp.game.Player;
 import ca.ulaval.ima.mp.game.roles.Role;
+import ca.ulaval.ima.mp.network.WSResponse;
 
 public class WinFragment extends AbstractFragment {
 
@@ -36,9 +51,49 @@ public class WinFragment extends AbstractFragment {
                 textYouAre.setText(textYouAre.getText().toString().replace("...", "Les Villageois"));
                 break ;
         }
+        sendStats();
         mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ((GameActivity)mContext).onBackPressed();
+            }
+        });
+    }
+
+    private void sendStats()
+    {
+        Game game = ((GameActivity)mContext).getGame();
+        JSONArray villagers = new JSONArray();
+        JSONArray werewolves = new JSONArray();
+        for (Player p : game.getPlayers())
+        {
+            if (p.getRole().getSide() == Role.Side.VILLAGER)
+                villagers.put(p.getName());
+            else if (p.getRole().getSide() == Role.Side.WEREWOLF)
+                werewolves.put(p.getName());
+        }
+        JSONObject params = new JSONObject();
+        try {
+            params.put("winningside", mWinner == Role.Side.VILLAGER ? "VILLAGERS" : "WEREWOLVES");
+            params.put("villagers", villagers);
+            params.put("werewolves", werewolves);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        SharedPreferences sharedPreferences = BlueGarouApplication.getInstance().getSharedPreferences("BLUEGAROU", Context.MODE_PRIVATE);
+        BlueGarouApplication.getInstance().getWebService().POSTRequestWithAuth("stats", onStats(), params, sharedPreferences.getString("access_token", ""));
+    }
+
+    private WSResponse<JSONObject> onStats() {
+        return new WSResponse<>(new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(final JSONObject response) {
+                Log.d("GET_OBJX", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("GET_OBJX", error.toString());
             }
         });
     }
